13d7c91eed0ce67339c5fd874a385f64b6cbcffb
Ian Kabaka <iankabaka1@gmail.com>
2024-12-29 19:31:11 +0300

    passing tests for dsa tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root 
+    def get_element_by_id(self, target_id):
+        def dfs(node):
+         
+            if node['id'] == target_id:
+                return node
 
-  def get_element_by_id(self, id):
-    pass
+            for child in node['children']:
+                result = dfs(child)
+                if result:
+                    return result
+            
+            return None  
+       
+        return dfs(self.root)
